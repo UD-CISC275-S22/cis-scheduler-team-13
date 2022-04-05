b381cb80abd794fde99703054597f73343725846
dehua123 <mheider@udel.edu>
2022-04-05 15:38:10 -0400

    added names to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Names of Group 13: Clay Wilfong Mathias Heider Brandon Grier
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
