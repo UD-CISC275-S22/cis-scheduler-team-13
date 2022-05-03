53461a044266dca4825be75681d789cc5450477c
Brandon Grier <bgrier@udel.edu>
2022-05-02 21:17:04 -0400

    listsAdded

---
+++ src/lists/multicultCourses.ts
@@ -0,0 +1,301 @@
+import { course } from "../interface/course";
+export function multicultCourses(): course[] {
+    const multicultCourses: course[] = [
+        {
+            code: "AFRA 110",
+            name: "Introduction to African American Studies",
+            descr: "Assesses the status of Black America in the modern technological order through an examination of the major spheres of institutional life, including employment, education politics, criminal justice and the military.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall and Spring"
+        },
+        {
+            code: "AFRA 193",
+            name: "Special Topics: Re(de)constructing AfroLatinx Intersections",
+            descr: "This course takes a multidisciplinary approach to the study of peoples of African descent in Caribbean and Latin American context. This course begins with continental Africa from early times (concerning the TransAtlantic Slave Trade and continues to contemporary times. The course reviews the geography, history, social, political, and economic developments of peoples of African descent in the Caribbean and Latin America. It also reviews the evolution and implication of the Atlantic slave trade and the dispersal of peoples of African descent in the Caribbean and Latin American realm and the effects of enslavement involving cultural development.",
+            credits: " 1-3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall, Winter and Spring"
+        },
+        {
+            code: "AFRA 205",
+            name: "Contemporary African American Issues",
+            descr: "The impact of important issues involving African American people in contemporary American society. Critical theoretical analyses of trends that affect programs of the Black community writ large.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Social and Behavioral Sciences; A&S: GROUP C: A&S Social & Behavioral Sci",
+            typ: "Fall"
+        },
+        {
+            code: "AFRA 206",
+            name: "Survey of African American Culture",
+            descr: "Historic and contemporary aspects of African American culture. A multi-faceted approach utilizing resources from music, literature, history, folklore, religion and sociology.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Creative Arts and Humanities; A&S: GROUP A: A&S Creative Arts & Humanities",
+            typ: "Spring"
+        },
+        {
+            code: "AFRA 220",
+            name: "The Civil Rights Movement",
+            descr: "This course examines African American struggles for freedom, equality, and citizenship from the 1940s to the present.",
+            credits: " 3-4",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Verify offering with Dept"
+        },
+        {
+            code: "AFRA 221",
+            name: "Race and Sports in American Society",
+            descr: "",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change (HIST &amp; CLT); A&S: ",
+            typ: "Fall and Spring"
+        },
+        {
+            code: "AFRA 222",
+            name: "Racial Politics in Today\u2019s Sports",
+            descr: "",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change (HIST &amp; CLT); A&S: ",
+            typ: "Winter and Summer"
+        },
+        {
+            code: "AFRA 223",
+            name: "Sports, Media, and Culture",
+            descr: "This course examines the interrelationship of sports, media, and culture in today\u2019s society and how that interrelationship sometimes reinforces or challenge social values. It draws on the cultural identification of class, race, and gender to identify sports values and cultural values. Drawing on theories of culture, rhetoric, and social criticism, we will examine media\u2019s role in telling the story of sports and, in telling that story, shapes cultural values. Students will study several critical approaches to sports and public discourse and will apply those approaches to journalism, public relations, the news and popular media, and other forms of mass communication.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Social and Behavioral Sciences; A&S: GROUP C: A&S Social & Behavioral Sci",
+            typ: "Fall, Winter, Spring, Summer"
+        },
+        {
+            code: "AFRA 225",
+            name: "Black Male Experience",
+            descr: "",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Social and Behavioral Sciences (SOC &amp; BESC); A&S: GROUP C: A&S Social & Behavioral Sci",
+            typ: "Fall and Spring"
+        },
+        {
+            code: "AFRA 250",
+            name: "Afro Future Females",
+            descr: "This course focuses on visual, literary, and sonic representations of black womanhood of the 20<sup>th</sup> and 21<sup>st</sup> centuries in the context of black woman-authored speculative fiction and discourses of futurity. Through closely looking at and listening to visual, literary, and sonic texts by black women, we will consider social constructions of race, gender, and sexuality; the significance of self-fashioning; and notions of community and belonging.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Creative Arts and Humanities; A&S: GROUP A: A&S Creative Arts & Humanities",
+            typ: "Fall and Spring"
+        },
+        {
+            code: "AFRA 251",
+            name: "Addressing Inequity (Social, Political, Economic, Educational)",
+            descr: "Through an interdisciplinary lens this course explores current social, political, educational and economic inequities facing the black community and critically assess their causes, effects, and possible solutions (i.e. disenfranchisement, the school-to-prison pipeline, mass incarceration, policing, and poverty).",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth: "University: Social and Behavioral Sciences; A&S: ",
+            typ: ""
+        },
+        {
+            code: "AFRA 304",
+            name: "African American History to the Civil War",
+            descr: "The history of African American life and culture to the Civil War.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall"
+        },
+        {
+            code: "AFRA 305",
+            name: "Black Media Cultures",
+            descr: "Evaluate various forms of media by black cultural producers that prompt nuanced understandings of black social life, from documentary and mainstream film to Black Twitter to video games.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Creative Arts and Humanities; A&S: GROUP A: A&S Creative Arts & Humanities",
+            typ: ""
+        },
+        {
+            code: "AFRA 306",
+            name: "African America History Since the Civil War",
+            descr: "Provides an overview of the major themes, issues, and debates in African American History from the Civil War to the contemporary era. Through readings, lectures, discussions, and films, the course will address: the process of emancipation, Reconstruction, Jim Crow segregation and racial violence, black cultural production, the modern black freedom struggles, and black life in the 21st century.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Spring"
+        },
+        {
+            code: "AFRA 314",
+            name: "International Black Design - 20th Century to Now",
+            descr: "This course looks at the breadth and depth of international Black design from the 20th century to contemporary times, concentrating on African American designers. Using historical and cultural analysis to place designed objects in their context, students understand the symbiotic relationships among design, politics, history, and culture",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Creative Arts and Humanities; A&S: GROUP A: A&S Creative Arts & Humanities",
+            typ: "Spring"
+        },
+        {
+            code: "AFRA 315",
+            name: "Issues in Black Photography",
+            descr: "A critical race history of blacks as subjects, producers, and theorists of photography. Includes examination of issues, ideas, and creativity with respect to African American and African diasporic photography from the late nineteenth century to the present day. Topics range from portraiture and documentary photography to considerations of race and representation, black consciousness, strategies of resistance and identity formation, class, sex, and gender.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Creative Arts and Humanities; A&S: GROUP A: A&S Creative Arts & Humanities",
+            typ: ""
+        },
+        {
+            code: "AFRA 318",
+            name: "Feminism(s) and Fashion in the African Diaspora",
+            descr: "Examine classic black feminist texts and cutting-edge scholarship on body politics and popular culture. Together, these materials will illuminate the vibrancy and diversity of fashion in the African diaspora emphasizing its political implications. Our exploration of underground and mainstream fashion cultures just might change what we think we know about black feminism and its cultural-political legacies.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Creative Arts and Humanities; A&S: GROUP A: A&S Creative Arts & Humanities",
+            typ: "Fall"
+        },
+        {
+            code: "AFRA 320",
+            name: "History and Significance of Race in America",
+            descr: "",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History &amp; Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall and Spring"
+        },
+        {
+            code: "AFRA 321",
+            name: "Black Women and Popular Culture",
+            descr: "Black women are a force in U.S. popular culture. This course introduces students to cutting-edge black feminist and queer theory as it examines representations of black women in contemporary media culture.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Social and Behavioral Sciences; A&S: GROUP C: A&S Social & Behavioral Sci",
+            typ: "Fall"
+        },
+        {
+            code: "AFRA 325",
+            name: "Contemporary Black Art",
+            descr: "Examines the art and visual culture of the African diaspora since the 1960s. Topics include the Black Arts Movement in the USA and UK, Abstract Expressionism, the Afri-Cobra movement, recycled stereotypes, installation art, and performance art.",
+            credits: " 3",
+            preReq: "May be crosslisted with ARTH 345.",
+            restrict: "",
+            breadth: "University: Creative Arts and Humanities; A&S: ",
+            typ: ""
+        },
+        {
+            code: "AFRA 329",
+            name: "Black Women\u2019s History to 1865",
+            descr: "This course brings into focus the lived experiences of Black women throughout United States history through the end of the Civil War. Beginning with the first African women to encounter North American shores in the 17<sup>th</sup> century, Black women\u2019s knowledge, creativity, activism, and community leadership have been integral to both Black people\u2019s perseverance, and to the evolution of this country. Throughout this course, Black women\u2019s voices will be central. We will read Black women\u2019s writing, examine the ways Black women fashioned their own bodies, and we will survey Black women\u2019s art throughout this early period. We will examine a wide range of sources including periodicals, slave narratives, memoir/autobiography, oral history, poetry, visual art, film, and music, in addition to assigned secondary literature. In doing so, this course surveys the multitude of Black women\u2019s experiences and perspectives across class, region, and time. We will move both chronologically and thematically as we explore core themes such as labor, motherhood, sisterhood, sexuality, resistance, respectability, and political activism. A primary goal will be to consider how Black women have worked to determine their own lives and possess their own bodies while other groups have sought to dominate and control them.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall"
+        },
+        {
+            code: "AFRA 334",
+            name: "Black Women\u2019s History Since 1865",
+            descr: "Explores the diversity of African American women\u2019s lives and development of women, work, and culture from 1865 through the late 20th century. Examines the social, political, religious and economic factors affecting change and transformation in the lives of African American women. Provides a broad introduction to the interdisciplinary field of African American and Women\u2019s Studies.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Verify offering with Dept"
+        },
+        {
+            code: "AFRA 362",
+            name: "Maroons and Marronage in the Atlantic World",
+            descr: "This course examines the hemispheric history of maroons and marronage in slaveholding Latin America, the Caribbean, and the United States. Maroons and their communities, or communities of those some would call \u201crunaway slaves,\u201d were an ever-present feature of slaveholding societies throughout the Americas. Everywhere they existed, from Brazil to Jamaica, from Virginia to Suriname, or from Mexico to Haiti, they proved the indomitable spirit of African people and their descendants, and the great failures of the institution of slavery. Throughout the course, we will look closely at the demographic, economic, and geographic opportunities for enslaved mobility and resistance that shaped the formation of the Atlantic world. The central focus will be an analysis of the historical impact of marronage across the Americas from 1502 to 1865.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall"
+        },
+        {
+            code: "ANFS 100",
+            name: "Animals and Human Culture",
+            descr: "Animals have important roles in human society and their significance varies across diverse cultural settings. Explore human-animal interactions on issues related to food and fiber production, welfare, conservation, research, work and service, natural and man-made disasters, zoonotic disease, and human health.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth: "University: Social and Behavioral Sciences; A&S: ",
+            typ: ""
+        },
+        {
+            code: "ANFS 102",
+            name: "Food for Thought",
+            descr: "Overview of our foods today including how the composition, safety, processing, and chemistry influence our choices and overall production of food. The effects of social influences and marketing of foods will be discussed as these are important consideration for individuals and food companies.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: Mathematics, Natural Sciences and Technology; A&S: GROUP D: A&S Math, Nat Sci & Technology",
+            typ: "Spring"
+        },
+        {
+            code: "ANTH 101",
+            name: "Introduction to Social and Cultural Anthropology",
+            descr: "Major ideas and areas of study in social and cultural anthropology. Use of ethnographic data and film to illustrate the anthropologist\u2019s view of societies in their sociocultural and ecological dimensions.",
+            credits: " 3",
+            preReq: "",
+            restrict: "",
+            breadth:
+                "University: History and Cultural Change; A&S: GROUP B: A&S History & Cultural Change",
+            typ: "Fall and Spring"
+        },
+        {
+            code: "ANTH 103",
+            name: "Introduction to Prehistoric Archaeology",
+            descr: "Fundamental concepts and research procedures of archaeology. History ofarchaeology, methods of fieldwork and artifact analysis, and theories of humanbehavior in archaeological research.",
+            credits: " 3",
+            preReq: "",
+            restrict:
+                "Students who received credit in ANTH104 are not eligible to take this course without permission.",
+            breadth:
+                "University: Social and Behavioral Sciences; A&S: GROUP C: A&S Social & Behavioral Sci",
+            typ: "Fall"
+        }
+    ];
+    return multicultCourses;
+}
